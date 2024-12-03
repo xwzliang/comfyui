b3d124540d061a3f729c60bf612092a12f2736e5
broliang <18706703437@163.com>
2024-12-03 11:33:28 +0800

    Fix.

--- config/provisioning/cogvideox.sh
+++ config/provisioning/cogvideox.sh
@@ -125,6 +125,7 @@ function provisioning_start() {
     provisioning_get_nodes
     provisioning_get_frame_interpolation
     provisioning_get_pip_packages
+    provisioning_get_custom_models
     provisioning_get_models \
         "${WORKSPACE}/storage/stable_diffusion/models/ckpt" \
         "${CHECKPOINT_MODELS[@]}"
