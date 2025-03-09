c85a405069038b0a8f558b599e14fc85d2893dd6
broliang <18706703437@163.com>
2025-03-09 17:11:44 +0800

    update

--- config/provisioning/whisperx.sh
+++ config/provisioning/whisperx.sh
@@ -131,6 +131,7 @@ function provisioning_start() {
     provisioning_get_pip_packages
     provisioning_get_custom_nodes
     pip install hf_transfer
+    pip install ctranslate2==4.4.0
     provisioning_get_custom_model_repos
     provisioning_get_huggingface_repos_to_cache
     provisioning_get_custom_models
